4b428db22df63df7ba261180c90bfcf03b9f4d4d
elinaols <elol0031@student.hv.se>
2025-03-24 10:27:37 +0100

    Updated with post

--- routes/recipies.js
+++ routes/recipies.js
@@ -12,4 +12,33 @@ router.get('/', function(req, res, next) {
     })
 });
 
+// Creates a new recepie and saves it to the database
+router.post('/', function(req, res, next) {
+    //req.body är innehållet i requestobjektet, dvs en json med en bil
+    RecipiesModel.create(req.body).then(function(post) {
+        res.json(post); //Här skickar vi tillbaka datan vi skickar in i databasen, om skrivningen gick bra
+    }); 
+});
+
+/*
+router.delete('/:id', function(req, res, next) {
+    RecipiesModel.findByIdAndDelete(req.params.id, req.body).then(function(post) {
+    res.json(post);
+    });
+});
+// https://www.youtube.com/watch?v=_7UQPve99r4 - inspiration
+// https://stackoverflow.com/questions/40330916/updating-a-record-with-mongoose - recommends CarModel.findByIdAndUpdate({"_id": req.params.id}, req.body)
+// don´t now if it works
+router.put('/:id', function(req, res, next){
+    const {id} = req.params;
+    RecipiesModel.findByIdAndUpdate(id, req.body)
+    .then(function(post){
+        res.json(post)
+    })
+    .catch(function(error) {
+        console.log(error)
+        next(error)
+    })
+})
+*/
 module.exports = router;
